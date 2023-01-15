2528a0f5b1cd2a7396eb8e6ab516c88aaf297f3e
marcos luciano <marcos3dt@gmail.com>
2023-01-15 04:30:50 -0300

    criação de class e metodos do CRUD

--- src/test/estrategia01/ContaTestWeb.java
+++ src/test/estrategia01/ContaTestWeb.java
@@ -1,2 +1,68 @@
-package estrategia01;public class ContaTestWeb {
+package estrategia01;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ContaTestWeb {
+
+    private ChromeDriver driver;
+    @Before
+    public void login(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+        driver.get("http://seubarriga.wcaquino.me");
+        driver.findElement(By.id("email")).sendKeys("marcos3dt@marcos");
+        driver.findElement(By.id("senha")).sendKeys("123");
+        driver.findElement(By.tagName("button")).click();
+    }
+    @Test
+    public void test_01_inserir(){
+        driver.findElement(By.linkText("Contas")).click();
+        driver.findElement(By.linkText("Adicionar")).click();
+        driver.findElement(By.id("nome")).sendKeys("ContaEstrategicaDeMassa01");
+        driver.findElement(By.tagName("button")).click();
+        String msgAlert = driver.findElement(By.xpath("//div[@class = 'alert alert-success']")).getText();
+        Assert.assertEquals("Conta adicionada com sucesso!", msgAlert);
+
+    }
+    @Test
+    public void  test_02_Cosultar(){
+        driver.findElement(By.linkText("Contas")).click();
+        driver.findElement(By.linkText("Listar")).click();
+        driver.findElement(By.xpath("//td[contains(text(),'ContaEstrategicaDeMassa01')]/..// a")).click();
+        String nomeConta = driver.findElement(By.id("nome")).getAttribute("value");
+        Assert.assertEquals("ContaEstrategicaDeMassa01", nomeConta);
+    }
+    @Test
+    public void  test_03_Alterar(){
+        driver.findElement(By.linkText("Contas")).click();
+        driver.findElement(By.linkText("Listar")).click();
+        driver.findElement(By.xpath("//td[contains(text(),'ContaEstrategicaDeMassa01')]/..// a")).click();
+        driver.findElement(By.id("nome")).clear();
+        driver.findElement(By.id("nome")).sendKeys("ContaEstrategicaDeMassa01_ALTERADA");
+
+        driver.findElement(By.tagName("button")).click();
+        String msgAlert = driver.findElement(By.xpath("//div[@class = 'alert alert-success']")).getText();
+        Assert.assertEquals("Conta alterada com sucesso!", msgAlert);
+    }
+    @Test
+    public void text_04_remover(){
+        driver.findElement(By.linkText("Contas")).click();
+        driver.findElement(By.linkText("Listar")).click();
+        driver.findElement(By.xpath("//td[contains(text(),'ContaEstrategicaDeMassa01')]/..// a[2]")).click();
+        String msgAlert = driver.findElement(By.xpath("//div[@class = 'alert alert-success']")).getText();
+        Assert.assertEquals("Conta removida com sucesso!", msgAlert);
+
+
+    }
+
+    //td[contains(text(),'ContaEstrategicaDeMassa01')]/..// a//td[contains(text(),'ContaEstrategicaDeMassa01')]/..// a
+    @After
+    public void fechar(){
+        driver.quit();
+    }
 }
--- src/test/estrategia01/UsuarioServicoTest.java
+++ src/test/estrategia01/UsuarioServicoTest.java
@@ -1,2 +1,48 @@
-package estrategia01;public class UsuarioServicoTest {
+package estrategia01;
+
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.service.UsuarioService;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class UsuarioServicoTest {
+    //variaveis globais
+    private UsuarioService servico = new UsuarioService();
+    private static Usuario usuarioGlobal;
+
+
+    @Test
+    public void test1_inserir() throws Exception {
+
+        Usuario usuario = new Usuario("Usuario estrategia #1", "marcos3dt@marcos", "123");
+        usuarioGlobal = servico.salvar(usuario);
+        Assert.assertNotNull(usuarioGlobal.getId());
+
+    }
+
+    @Test
+    public void test2_consultar() throws Exception {
+        Usuario usuario = servico.findById(usuarioGlobal.getId());
+        Assert.assertEquals("Usuario estrategia #1", usuario.getNome());
+
+    }
+
+    @Test
+    public void test3_alterar() throws Exception {
+        Usuario usuario = servico.findById(usuarioGlobal.getId());
+        usuario.setNome("Usuário Alterado");
+        Usuario usuarioAlterado = servico.salvar(usuario);
+        Assert.assertEquals("Usuário Alterado", usuarioAlterado.getNome());
+    }
+
+    @Test
+    public void test4_excluir() throws Exception {
+        servico.delete(usuarioGlobal);
+        Usuario usuarioRemovido = servico.findById(usuarioGlobal.getId());
+        Assert.assertNull(usuarioRemovido);
+    }
+
 }
